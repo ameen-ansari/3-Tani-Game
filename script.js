e463ab6f9b09a1177707c82cd37f8ae4d148e2c8
M.Ameen <ameen55668@gmail.com>
2022-08-17 14:05:27 +0500

    add music & comments

--- script.js
+++ script.js
@@ -1,31 +1,41 @@
 // Change the Inner Text
-
-var a = [1]
-var xyz;
 var displayTurn = document.getElementById('turn')
 var winner = document.getElementById('indicate')
 var items = document.getElementsByClassName('item')
 var part2 = document.getElementsByClassName('part2')[0]
 var part1 = document.getElementsByClassName('part1')[0]
+var music = new Audio("music.mp3")
+var bgmusic = new Audio("bg-music.mp3")
 // var user1 = prompt('Please inter the First user name')
 // var user2 = prompt('Please inter the Second user name')
 var user1 = 'x'
 var user2 = 'y'
 displayTurn.innerText = user1
-var turn;
+
+// Main Variables
+var turn
+var a = [1]
+var xyz
 var innertextc = (e) => {
   if (e.innerText === '') {
     if (a.length === 1) {
       turn = 'x'
       a.length++
       displayTurn.innerText = user2
+      music.play()
+      bgmusic.play()
     } else if (a.length !== 1) {
+      bgmusic.play()
+      music.play()
+      // bgmusic.play()
       turn = 0
       a.length--
       displayTurn.innerText = user1
     }
     e.innerText = turn
   }
+
+  // make array to get the result
   var array = [
     [0, 1, 2],
     [3, 4, 5],
@@ -36,17 +46,18 @@ var innertextc = (e) => {
     [1, 4, 7],
     [2, 5, 8],
   ]
+  // Target Every Element & Show the Results in new HTML Tag
   var results = () => {
     array.forEach((e) => {
       if (
         (items[e[0]].innerText === '0' &&
-          items[e[1]].innerText === '0' &&
-          items[e[2]].innerText === '0') ||
+        items[e[1]].innerText === '0' &&
+        items[e[2]].innerText === '0') ||
         (items[e[0]].innerText === 'x' &&
-          items[e[1]].innerText === 'x' &&
-          items[e[2]].innerText === 'x')
-      ) {
-        if (
+        items[e[1]].innerText === 'x' &&
+        items[e[2]].innerText === 'x')
+        ) {
+          if (
           items[e[0]].innerText === '0' &&
           items[e[1]].innerText === '0' &&
           items[e[2]].innerText === '0'
@@ -78,21 +89,22 @@ playagain = () => {
   })
   a = [1]
   winner.innerHTML = `<b>${xyz} won the last time</b></br>`
-          displayTurn.innerText = xyz
+  displayTurn.innerText = xyz
 }
 
-// logic for changeusername
+// logic for Display & Hide the HTML Elements
 var input1 = document.getElementById('input1')
 var input2 = document.getElementById('input2')
 var setname = document.getElementById('setname')
 changeusername = () => {
-  if(part2.style.display !== "none"){
-    part2.style.display = "none"
+  if (part2.style.display !== 'none') {
+    part2.style.display = 'none'
   }
   if (setname.style.display !== 'flex') {
     setname.style.display = 'block'
   }
 }
+// logic for changeusername
 set = () => {
   user1 = input1.value
   user2 = input2.value
@@ -102,8 +114,7 @@ set = () => {
   if (setname.style.display === 'block') {
     setname.style.display = 'none'
   }
-  if(part2.style.display !== "block"){
-    part2.style.display = "block"
+  if (part2.style.display !== 'block') {
+    part2.style.display = 'block'
   }
-  
 }
